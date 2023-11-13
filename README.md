9392c4be3a9e0a36a70daf12bd05dcf96fdd582c
alireza.rahnama <alirezarm3@gmail.com>
2023-11-13 20:09:08 -0330

    new features and updated demo!

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 A new Flutter project.
 # dear_diary_with_cloud_firebase_authentication_database_storage
 
-Find the application's demo on youtube at [https://youtu.be/CK4Mw7iYlcY].
+Find the application's demo on [Youtube] (https://youtu.be/K8IjSxE23rQ).
---
+++ lib/view/diary_entry_view2.dart
@@ -0,0 +1,273 @@
+// import 'package:dear_diary_with_hive/controller/diary_entry_service.dart';
+// import 'package:dear_diary_with_hive/model/diary_model.dart';
+import 'package:dear_diary_with_firebase_auth_storage_database/view/diary_list_view2.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import '../controller/diary_entry_service.dart';
+import '../controller/diary_entry_service2.dart';
+import '../model/diary_model.dart';
+import '../model/diary_model2.dart';
+import 'diary_list_view..dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
+class NewEntryView2 extends StatefulWidget {
+  bool isDark;
+
+  NewEntryView2.withInheritedTheme(this.isDark);
+
+  @override
+  // _NewEntryViewState createState() => _NewEntryViewState();
+  _NewEntryViewState createState() =>
+      _NewEntryViewState.withInheritedTheme(isDark);
+}
+
+class _NewEntryViewState extends State<NewEntryView2> {
+  final TextEditingController descriptionController = TextEditingController();
+  double rating = 3.0; // Initial rating value
+  DateTime selectedDate = DateTime.now(); // Initial date value
+  late String description;
+  String? imagePath;
+  List<String?> imagePathList = [];
+  var diaryController = DiaryController2();
+  final ImagePicker _picker = ImagePicker();
+  XFile? _image;
+  late bool isDark;
+
+  _NewEntryViewState.withInheritedTheme(bool isDark) {
+    this.isDark = isDark;
+  }
+
+  Future<void> _pickImageFromGallery() async {
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      _image = image;
+    });
+  }
+
+  Future<void> _pickImageFromCamera() async {
+    final XFile? image = await _picker.pickImage(source: ImageSource.camera);
+    setState(() {
+      _image = image;
+    });
+  }
+
+  Future<String?> _uploadImageToFirebaseAndReturnDownlaodUrl() async {
+    if (_image == null) return null;
+    String? downloadURL = null;
+    final currentUser = FirebaseAuth.instance.currentUser;
+    if (currentUser == null) return null;
+    final firebaseStorageRef = FirebaseStorage.instance
+        .ref()
+        .child('images/${currentUser.uid}/${_image!.name}');
+
+    try {
+      final uploadTask = await firebaseStorageRef.putFile(File(_image!.path));
+      if (uploadTask.state == TaskState.success) {
+        downloadURL = await firebaseStorageRef.getDownloadURL();
+
+        print("Uploaded to: $downloadURL");
+      }
+    } catch (e) {
+      print("Failed to upload image: $e");
+    }
+    return downloadURL;
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+
+    if (picked != null && picked != selectedDate)
+      setState(() {
+        selectedDate = picked;
+      });
+  }
+
+  void _saveDiaryEntry() async {
+    description = descriptionController.text;
+    imagePath = await _uploadImageToFirebaseAndReturnDownlaodUrl();
+    imagePathList.add(imagePath);
+
+    if (description == '') {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+            content: Center(child: Text('Description can not be left empty!',
+                style: TextStyle(
+                  color: Colors.white, // Customize the hint text color
+                  fontSize: 12, // Customize the hint text font size
+                ))),
+            backgroundColor: Colors.deepPurple),
+      );
+      return;
+    }
+    DiaryModel2 diaryEntry = DiaryModel2(
+        dateTime: selectedDate,
+        description: description,
+        rating: rating.toInt(),
+        imagePath: imagePath,
+        imagePathList: imagePathList);
+
+    bool successfullyAdded =
+    await diaryController.addDiaryWithDateCheck(diaryEntry);
+
+    descriptionController.clear();
+    print('rating is: $rating');
+    try {
+      if (successfullyAdded) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+              content: Center(child: Text('Entry successfully saved!',
+                  style: TextStyle(
+                    color: Colors.white, // Customize the hint text color
+                    fontSize: 12, // Customize the hint text font size
+                  ))),
+              backgroundColor: Colors.deepPurple),
+        );
+        print('isDark in diary entry view is: $isDark');
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => DiaryLogView2.WithPersistedTheme(isDark)),
+        );
+
+      } else if (!successfullyAdded) {
+        // Show the error dialog when the button is pressed
+        showDialog(
+          context: context,
+          builder: (context) {
+            return ErrorDialog();
+          },
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            content: Text('Unexpected error ocured: $e'),
+            backgroundColor: Colors.deepPurple),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData themeData = ThemeData(
+        useMaterial3: true,
+        brightness: isDark ? Brightness.dark : Brightness.light);
+
+    return Theme(
+        data: themeData,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.deepPurple,
+            title: Text("Add Diary Entry",
+                style: GoogleFonts.pacifico(
+                  color: isDark? Colors.black87 : Colors.white,
+                  fontSize: 30.0,
+                )),
+            leading: IconButton(
+                icon: Icon(Icons.arrow_back_outlined),
+                tooltip: 'Go back',
+                color: isDark? Colors.black87 : Colors.white,
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(builder: (context) => DiaryLogView()),
+                  // );
+                }),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextField(
+                  controller: descriptionController,
+                  decoration: InputDecoration(
+                    labelText: 'Description',
+                    hintText: 'Enter your description (max 140 characters)',
+                  ),
+                  maxLength: 140, // Set the maximum character limit
+                  maxLines: null, // Allow multiple lines of text
+                ),
+                Text('Enter your description (max 140 characters)',
+                    style: TextStyle(
+                      color: Colors.grey, // Customize the hint text color
+                      fontSize: 12, // Customize the hint text font size
+                    )),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('Rate Your Day: ${rating.toInt()} Stars'),
+                    Slider(
+                      value: rating,
+                      min: 1,
+                      max: 5,
+                      onChanged: (newRating) {
+                        setState(() {
+                          rating = newRating;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('Date: ${selectedDate.toLocal()}'.split(' ')[0]),
+                    ElevatedButton(
+                      onPressed: () => _selectDate(context),
+                      child: Text(
+                          '${DateFormat('yyyy-MM-dd').format(selectedDate)}'),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: _pickImageFromGallery,
+                  child: Text('Add Image from Gallery'),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: _pickImageFromCamera,
+                  child: Text('Add Image from Camera'),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: _saveDiaryEntry,
+                  child: Text('Save Entry'),
+                ),
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
+
+class ErrorDialog extends StatelessWidget {
+  const ErrorDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Error Exception'),
+      content: const Text('Entry already exists for this date!'),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () => Navigator.pop(context, 'OK'),
+          child: const Text('OK'),
+        ),
+      ],
+    );
+  }
+}
